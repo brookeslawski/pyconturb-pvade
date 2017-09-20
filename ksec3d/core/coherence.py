d0f4378cdb6ea4a426cc8cf8215bc357ff95f665
Jennifer Rinker <rink@win.dtu.dk>
2017-09-20 09:23:58 +0200

    added more comments

--- ksec3d/core/coherence.py
+++ ksec3d/core/coherence.py
@@ -11,6 +11,11 @@ def get_coherence(k1, k2, x1, x2, freq,
 
     Calls coherence-model-specific subfunctions.
 
+    Notes
+    -----
+    The spatial coordinate system is x positive to the right, z positive up,
+    and y positive downwind.
+
     Parameters
     ----------
     k1 : int
@@ -27,12 +32,12 @@ def get_coherence(k1, k2, x1, x2, freq,
         Coherence model to use. Default is IEC Ed. 3.
     """
 
-    if coh_model == 'iec':
-        if 'ed' not in kwargs.keys():  # add IEC edition if not passed in
+    if coh_model == 'iec':  # IEC coherence model
+        if 'ed' not in kwargs.keys():  # add IEC ed to kwargs if not passed in
             kwargs['ed'] = 3
         coh = get_iec_coherence(k1, k2, x1, x2, freq, **kwargs)
 
-    else:
+    else:  # unknown coherence model
         raise ValueError(f'Coherence model "{coh_model}" not recognized.')
 
     return coh
@@ -41,6 +46,11 @@ def get_coherence(k1, k2, x1, x2, freq,
 def get_iec_coherence(k1, k2, x1, x2, freq,
                       **kwargs):
     """Exponential coherence specified in IEC 61400-1
+
+    Notes
+    -----
+    The spatial coordinate system is x positive to the right, z positive up,
+    and y positive downwind.
 
     Parameters
     ----------
@@ -61,16 +71,16 @@ def get_iec_coherence(k1, k2, x1, x2, freq,
     x1 = np.asarray(x1)  # convert locations to arrays in case they weren't
     x2 = np.asarray(x2)  # passed in as such
 
-    if kwargs['ed'] != 3:
+    if kwargs['ed'] != 3:  # only allow edition 3
         raise ValueError('Only edition 3 is permitted.')
-    if any([k not in kwargs.keys() for k in ['v_hub', 'l_c']]):
+    if any([k not in kwargs.keys() for k in ['v_hub', 'l_c']]):  # check kwargs
         raise ValueError('Missing keyword arguments for IEC coherence model')
 
-    r = np.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)
-    if (k1 == 0) and (k2 == 0):
+    r = np.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)  # inter-pt distance
+    if (k1 == 0) and (k2 == 0):  # u at point 1 and u at point 2
         coh = np.exp(-12 * np.sqrt((freq * r / kwargs['v_hub'])**2 +
                                    (0.12 * r / kwargs['l_c'])**2))
-    else:
+    else:  # any other combination of turbulence components is no coherence
         coh = 0
 
     return coh
