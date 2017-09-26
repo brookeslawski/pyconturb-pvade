5e2e9df339e8a4c0c3aabb544b8cac1372aecd69
Jennifer Rinker <rink@win.dtu.dk>
2017-09-26 16:13:55 +0200

    finalizing simulation for iec

--- ksec3d/core/simulation.py
+++ ksec3d/core/simulation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Functions related to the simulation of turbulence
 """
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -10,7 +11,8 @@ from .spectra import get_spectrum
 
 
 def gen_turb(spat_df,
-             coh_model='iec', spc_model='kaimal', T=600, dt=0.1, **kwargs):
+             coh_model='iec', spc_model='kaimal', T=600, dt=0.1,
+             seed=False, **kwargs):
     """Generate turbulence box
     """
 
@@ -23,15 +25,20 @@ def gen_turb(spat_df,
                             T=T, dt=dt, **kwargs)
 
     # create dataframe with phases
-    pha_df = get_phases(spat_df, coh_model=coh_model, spc_model=spc_model,
-                        T=T, dt=dt, **kwargs)
+    pha_df = get_phasors(spat_df,
+                         coh_model='iec', T=T, dt=dt,
+                         seed=seed,
+                         **kwargs)
 
     # multiply dataframes together
-    turb_fft = mag_df * np.exp(1j * pha_df)
+    turb_fft = mag_df * pha_df
 
-    # inverse fft
+    # set zero-frequency component to zero
+    turb_fft.iloc[:, 0] = 0
+
+    # inverse fft and transpose to utilize pandas functions easier
     turb_df = pd.DataFrame(np.fft.irfft(turb_fft, axis=1),
-                           columns=t)
+                           columns=t).T
 
     return turb_df
 
@@ -47,15 +54,58 @@ def get_magnitudes(spat_df,
     return np.sqrt(spc_df / 2)
 
 
-def get_phases(spat_df,
-               coh_model='iec', T=600, dt=0.1, **kwargs):
-    """Create realization of phases with desired coherence
+def get_phasors(spat_df,
+                coh_model='iec', T=600, dt=0.1, seed=None,
+                **kwargs):
+    """Create realization of phasors with desired coherence
+
+    Notes
+    -----
+    A phasor is the correlated complex Fourier component that contains the
+    phase information, but not the magnitude information. The uncorrelated
+    phasors are magnitude 1, but the correlated phasors are not.
     """
 
-    n_f = np.ceil(T / dt)//2 + 1
-    freq = np.arange(n_f) / T
-    for i_df, (i, j) in enumerate([(i, j) for i in range(spat_df.shape[0])
-                                   for j in range(spat_df.shape[0])]):
-        inp_df.iloc[i_df, ['k1', 'x1', 'y1', 'z1']] = spat_df.iloc[i, :]
-        inp_df.iloc[i_df, ['k2', 'x2', 'y2', 'z2']] = spat_df.iloc[j, :]
-    coh_df = get_coherence(spat_df, freq, coh_model='iec', **kwargs)
+    n_f = int(np.ceil(T / dt)//2 + 1)  # no. of frequencies
+    freq = np.arange(n_f) / T  # frequency array
+    n_s = spat_df.shape[0]  # no. of spatial points
+
+    n_pairs = int(np.math.factorial(n_s) / 2 /
+                  np.math.factorial(n_s - 2))  # no. of combos
+    pair_df = pd.DataFrame(np.empty((n_pairs, 8)),
+                           columns=['k1', 'x1', 'y1', 'z1', 'k2', 'x2',
+                                    'y2', 'z2'])  # df input to coherence fcn
+
+    i_df = 0  # initialize counter
+    ii, jj = [], []  # use these index vectors later during cholesky decomp
+    for (i, j) in itertools.combinations(spat_df.index, 2):
+        pair_df.loc[i_df, ['k1', 'x1', 'y1', 'z1']] = \
+            spat_df.loc[i, ['k', 'x', 'y', 'z']].values
+        pair_df.loc[i_df, ['k2', 'x2', 'y2', 'z2']] = \
+            spat_df.loc[j, ['k', 'x', 'y', 'z']].values
+        i_df += 1
+        ii.append(i)  # save index
+        jj.append(j)  # save index
+    coh_df = get_coherence(pair_df, freq, coh_model='iec', **kwargs)
+
+    np.random.seed(seed=seed)  # initialize random number generator
+    unc_pha = 2 * np.pi * np.random.rand(n_s, n_f)
+    pha_df = pd.DataFrame(np.empty((n_s, n_f)),
+                          columns=freq, dtype=complex)
+
+    for i_f in range(freq.size):
+        pha_df.iloc[:, i_f] = correlate_phasors(i_f, coh_df, unc_pha,
+                                                n_s, ii, jj)
+
+    return pha_df
+
+
+def correlate_phasors(i_f, coh_df, unc_pha, n_s, ii, jj):
+    """Correlate phasors
+    """
+    coh_mat = np.ones((n_s, n_s), dtype=complex)
+    coh_mat[ii, jj] = coh_df.iloc[:, i_f].values
+    coh_mat[jj, ii] = np.conj(coh_df.iloc[:, i_f].values)
+    cor_mat = np.linalg.cholesky(coh_mat)
+    cor_pha = np.dot(cor_mat, np.exp(1j * unc_pha[:, i_f]))
+    return cor_pha
---
+++ ksec3d/tests/test_simulation.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Test functions in coherence.py
+"""
+
+import numpy as np
+import pandas as pd
+
+from ksec3d.core.simulation import get_phasors
+
+
+def test_get_phases():
+    """Check the value for get_phases
+    """
+    # given
+    spat_df = pd.DataFrame([[0, 0, 0, 50],
+                            [0, 0, 0, 51]],
+                           columns=['k', 'x', 'y', 'z'])
+    kwargs = {'v_hub': 10, 'i_ref': 0.14, 'ed': 3, 'l_c': 340.2}
+    coh_model, T, dt, seed = 'iec', 8, 4, None
+    coh_theory = 0.8606565849
+    tol = 0.05  # stochastic tolerance
+    n_real = 100  # no. realizations for ensemble averaging
+
+    # when
+    coh = 0
+    den1 = 0
+    den2 = 0
+    for i_real in range(n_real):
+        phasors = get_phasors(spat_df,
+                              coh_model=coh_model, T=T, dt=dt,
+                              seed=seed,
+                              **kwargs)
+        Xi, Xj = phasors.iloc[0, 1], phasors.iloc[1, 1]
+        coh += Xi * np.conj(Xj)
+        den1 += Xi * np.conj(Xi)
+        den2 += Xj * np.conj(Xj)
+    corr = coh / np.sqrt(den1 * den2)
+
+    # then
+    assert (corr < coh_theory*(1 + tol)) & (corr > coh_theory*(1 - tol))
