d8811587c870ae0b0925d6e0e183096082a221b0
Jenni Rinker <rink@dtu.dk>
2019-09-23 11:52:02 +0200

    Fixing custom profiles and CI

--- pyconturb/magnitudes.py
+++ pyconturb/magnitudes.py
@@ -22,9 +22,15 @@ def spc_to_mag(spat_df, spc_arr, sig_func, **kwargs):
     # get unscaled magnitudes
     t, freq = get_freq(**kwargs)
     n_t, df = t.size, freq[1]
-    mags_arr = np.sqrt(spc_arr * df / 2)  # (nf, nsp)
-    mags_arr[0, :] = 0.  # mean is zero
+    mags = np.sqrt(spc_arr * df / 2)  # (nf, nsp)
+    mags[0, :] = 0.  # mean is zero to make math easier
     # scale to get the correct ti
-    std_arr = get_sig_values(spat_df, sig_func, **kwargs)  # (n_sp,)
-    alpha = std_arr / np.std(np.fft.irfft(mags_arr, n=n_t, axis=0) * n_t, axis=0)
-    return (alpha * mags_arr).astype(float)  # (nf, nsp)
+    std_theo = get_sig_values(spat_df, sig_func, **kwargs)  # (n_sp,)
+    if n_t == 2:
+        std_now = np.sqrt(n_t/(n_t-1) * np.sum(np.abs(mags)**2, axis=0))
+    else:
+        std_now = np.sqrt(n_t/(n_t-1)
+                          * (2*np.sum(np.abs(mags)**2, axis=0)
+                             - ((n_t+1) % 2)*np.abs(mags[-1, :])**2))
+    alpha = std_theo / std_now
+    return (alpha * mags).astype(float)  # (nf, nsp)
