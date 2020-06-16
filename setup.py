3229666905717d8363bd2b1c3fc3f37bbd5d0911
Jenni Rinker <rink@win.dtu.dk>
2020-06-16 19:59:10 +0200

    io no longer submodule

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(name='pyconturb',
       author_email='rink@dtu.dk',
       license='MIT',
       packages=['pyconturb',  # top-level package
-                'pyconturb.io',  # file io
                 ],
       install_requires=['numpy',  # numberic arrays
                         'pandas',  # column-labelled arrays
