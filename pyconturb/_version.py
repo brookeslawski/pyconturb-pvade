c94a958f9e32d44883e1a89f9ba0e6b32a99e53d
Jenni Rinker <rink@win.dtu.dk>
2020-11-27 17:32:20 +0100

    moving to 2.6.2

--- pyconturb/_version.py
+++ pyconturb/_version.py
@@ -1,5 +1,5 @@
 #   version:    1.0     First pass at PyConTurb
 #               2.0     Switch to uvw, other changes
 #                2.5    Fixed bug in custom profiles
-__version__ = '2.6.1'
-__release__ = '2.6.1'
+__version__ = '2.6.2'
+__release__ = '2.6.2'
