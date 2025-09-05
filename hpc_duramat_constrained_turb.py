10dabc142c5eafa099612e643c0cbc7efebd9dd9
brookeslawski <brooke.stanislawski@gmail.com>
2025-09-05 12:21:51 -0600

    converting to python script for running on hpc

--- hpc_duramat_constrained_turb.py
+++ hpc_duramat_constrained_turb.py
@@ -13,6 +13,25 @@ import h5py
 # copied from /Users/bstanisl/repos/pyconturb/pyconturb/duramat_constrained_turb.ipynb
 # to run on hpc terminal in hopes that it's faster
 
+# inputs
+save_turb_files_flag = True
+tf = 5.0 # 30.0 #s
+dt = 0.02 #s
+gen_csv_fname = 'generated_DuraMAT_tilt40deg_turbulent_inflow_{}s_{}Hz.csv'.format(int(tf),int(1/dt))
+
+# should match dimensions of pvade sim
+# y_min = -10.0
+# y_max = 10.0
+# z_max = 20.0
+# l_char = 0.17
+# ny = int((y_max-y_min)/l_char)
+
+# for testing
+y_min = -5.0
+y_max = 5.0
+z_max = 15.0
+ny = 11
+
 # Step 1: generate dataframe of measurement data ----------------------------
 # ---------------------------------------------------------------------------
 
@@ -21,111 +40,29 @@ x_sonic1 = 0.0
 y_sonic1 = 0.0
 z_sonic1 = 2.23 # m above ground
 
+# define spatial info of measurement data
 sonic_spat_df = gen_spat_grid(y_sonic1, z_sonic1)  # if `comps` not passed in, assumes all 3 components are wanted
-print('sonic_spat_df = ', sonic_spat_df.head())  # look at the first few rows
+# print('sonic_spat_df = ', sonic_spat_df.head())  # look at the first few rows
 
-# m2 tower data
-m2 = {}
-m2['u'] = [9.3218, 10.212, 11.211]
-m2['z'] = [2, 5, 10]
-m2['w_dir'] = [282.07, 275.67, 275.35]
-m2['Iu'] = [0.1542, 0.149, 0.1455]
+# Step 2: read csv of measurement data --------------------------------------
+# ---------------------------------------------------------------------------
 
-# def read_csv_data(sonic_data_fn, dt_sonic1):
-def read_csv_data(raw_data):
-    # read csv
-    # raw_data = pd.read_csv(sonic_data_fn, header=None)
-
-    # select only sonic data columns
-    sonic_data = raw_data[['u (m/s)', 'v (m/s)', 'w (m/s)']]
-
-    # downsample from 50 Hz to 4 Hz (sonic resolution)
-    # sonic_data = sonic_data.resample('250ms').median() # 4 Hz = 250 milliseconds
-
-    # construct time index
-    tmp = (sonic_data.index[1]-sonic_data.index[0])
-    dt_sonic1 = round(tmp.total_seconds(), 3)
-    # print(dt_sonic1)
-    tf_sonic1 = len(sonic_data) * dt_sonic1 # final time [s]
-    # print(tf_sonic1)
-    t_sonic1 = np.arange(0.0, tf_sonic1, dt_sonic1)
-
-    sonic_data = sonic_data.rename(columns={'u (m/s)':'u_p0', 'v (m/s)':'v_p0', 'w (m/s)':'w_p0'})
-    for col in sonic_data.filter(regex='u_', axis=1).columns:
-        sonic_data[col] = -1.0*sonic_data[col] # to make it positive from the west
-    sonic_data['index'] = t_sonic1
-    sonic_data = sonic_data.set_index('index')
-
-    return tf_sonic1, sonic_data
-
-# sonic_data_fn = 'sonic1_halfsecond_20Hz.csv'
-# sonic_data_fn = 'sonic1_5s_20Hz.csv'
-# sonic_data_fn = 'sonic1_20s_20Hz.csv'
-# sonic_data_fn = 'sonic1_10min_20Hz.csv'
-# sonic_data_fn = 'sonic1_10min_50Hz.csv'
-sonic_data_fn = 'DuraMAT_tilt40deg_turbulent_inflow_10min_timeseries.csv'
-
-raw_data = pd.read_csv(sonic_data_fn, index_col='Time')
-raw_data.index = pd.to_datetime(raw_data.index)
-# raw_data
-
-# tmp = (raw_data.index[1]-raw_data.index[0])
-# dt_sonic1 = round(tmp.total_seconds(), 3)
-
-tf_sonic1, sonic_data = read_csv_data(raw_data)
-dt = sonic_data.index[1]-sonic_data.index[0]
-print('dt = ', dt)
-print('sonic_data = ', sonic_data.head())
-
-# optional: cut down duration of signal for testing
-tf_sonic1 = 30.0 # [s] - this is also how long the synthetic turbulence signal will be
-sonic_data = sonic_data.loc[sonic_data.index <= tf_sonic1]
-
-fig, axs = plt.subplots()
-for col in sonic_data.columns:
-    plt.plot(sonic_data[col], marker='.', label=col)  # subselect long. wind component
-    # plt.plot(sonic_data[col].resample('50L').first(), marker='.', label=col)  # subselect long. wind component
-axs.set_ylabel('velocity [m/s]');
-plt.show()
-
-sonic_df = pd.concat([sonic_spat_df, sonic_data], axis=0)
-
-gen_csv_fname = 'generated_{}_{}s_{}Hz.csv'.format(sonic_data_fn[:34],int(tf_sonic1),int(1/dt))
-sonic_df.to_csv(gen_csv_fname)
-print(f'saved sonic data to csv {gen_csv_fname}')
-
-# Step 2: read in csv of measurement data ----------------------------
-# --------------------------------------------------------------------
-
+print(f'reading measured data from {gen_csv_fname} to generate con_tc')
 con_tc = TimeConstraint(pd.read_csv(gen_csv_fname, index_col=0))  # load data from csv directly into tc
-# con_tc = TimeConstraint(pd.read_csv('generated_sonic1_1s.csv', index_col=0))  # load data from csv directly into tc
 con_tc.index = con_tc.index.map(lambda x: float(x) if (x not in 'kxyz') else x)  # index cleaning
-con_tc.iloc[:7, :]  # look at the first 7 rows
+# print('con_tc = ', con_tc.iloc[:7, :])  # look at the first 7 rows
 
+# calc u_mean 
 time_df = con_tc.get_time()
-
-# for var in ['u_','v_','w_']:
-#     ax = time_df.filter(regex=var, axis=1).plot(lw=0.75)  # subselect long. wind component
-#     ax.set_ylabel(var + ' [m/s]');
-
-# [print(x) for x in time_df.filter(regex='u_', axis=1).mean()];  # print mean values
 u_mean_sonic1 = time_df.filter(regex='u_', axis=1).mean()
-print('u_mean_sonic1 = ', u_mean_sonic1)
+print(f'u_mean_sonic1 = {u_mean_sonic1.values[0]:.3f} m/s')
 
-# Step 3: generate constrained turbulence box ----------------------------
-# ------------------------------------------------------------------------
+# Step 3: define spatial info of generated turbulence -----------------------
+# ---------------------------------------------------------------------------
+y = np.linspace(y_min, y_max, ny)
 
-# to match pvade sim
-y_min = -30.0
-y_max = 30.0
-z_max = 20.0
-
-ny = 300 # 130 #41
-# nz = 60 #130 #20
-# y = np.linspace(-10.0, 10.0, ny) #11) #22)  # 11 lateral points from -50 to 50 (center @ 0)
-y = np.linspace(y_min, y_max, ny) #11) #22)  # 11 lateral points from -50 to 50 (center @ 0)
-# linearly increasing grid size in z
-steps = 0.15 + 0.02 * np.arange(60)
+# heights are linearly increasing as you move above the surface
+steps = 0.15 + 0.148 * np.arange(200) # 0.148 ensures a point at z_sonic = 2.23 m
 z = 0.0001 + np.cumsum(steps)
 z = z[z <= z_max]
 nz = len(z)
@@ -134,7 +71,86 @@ dy = y[1] - y[0] # (y[-1]-y[0])/ny
 dz = z[1] - z[0] # (z[-1]-z[0])/nz
 
 # this resolution should be approximately equal to or smaller than l_char
-print(f'dy = {dy:.3f} m, dz = {dz:.5f} m')
+print(f'ny = {ny}, nz = {nz}')
+print(f'dy = {dy:.3f} m, smallest dz = {dz:.5f} m')
 
 spat_df = gen_spat_grid(y, z)  # if `comps` not passed in, assumes all 3 components are wanted
-print(spat_df.head())  # look at the first few rows
+# print('spat_df = ',spat_df.head())  # look at the first few rows
+
+# Step 4: generate constrained turbulence -----------------------------------
+# ---------------------------------------------------------------------------
+fudge_factor = 2.1
+u_ref = float(u_mean_sonic1.values[0])
+z_ref = z_sonic1
+
+u_ref_sim = u_ref * fudge_factor
+print(f'generating turb, u_ref = {u_ref_sim:.3f} m/s, z_ref = {z_ref} m')
+
+kwargs = {'u_ref': u_ref_sim, 'turb_class': 'B', 'z_hub': z_ref,  # necessary keyword arguments for IEC turbulence
+      'T': con_tc.get_T(), 'nt': con_tc.get_time().index.size}  # simulation length (s) and time step (s)
+interp_data = 'none' # 'all'  # use the default IEC 61400-1 profile instead of interpolating from constraints
+
+# generate turbulence
+sim_turb_df = gen_turb(spat_df, con_tc=con_tc, interp_data=interp_data, verbose=True, **kwargs)
+
+# Step 5: save turbulence to csv file ---------------------------------------
+# ---------------------------------------------------------------------------
+sim_turb_fname = f'constrained_turb_ny{ny}_nz{nz}_sonic1_{int(tf)}s_u{round(u_mean_sonic1.values[0],3)}_{int(1/dt)}Hz.csv'
+if save_turb_files_flag:
+    sim_turb_df.to_csv(sim_turb_fname)
+    print(f'saved turbulenct to csv file: {sim_turb_fname}')
+
+# Step 6: check accurace of generated turbulence ----------------------------
+# ---------------------------------------------------------------------------
+
+# reshape to 3D array
+data = {}
+data['u'] = sim_turb_df.filter(regex='u').values.reshape(len(sim_turb_df),y.size,z.size).transpose((0, 2, 1))
+data['v'] = sim_turb_df.filter(regex='v').values.reshape(len(sim_turb_df),y.size,z.size).transpose((0, 2, 1))
+data['w'] = sim_turb_df.filter(regex='w').values.reshape(len(sim_turb_df),y.size,z.size).transpose((0, 2, 1))
+
+# print out error as a check
+j = np.argmin(abs(y - y_sonic1))
+k = np.argmin(abs(z - z_sonic1))
+
+print(f'comparing timeseries at (y,z) = ({y[j]:.2f},{z[k]:.2f}) to sonic loc of ({y_sonic1:.2f},{z_sonic1:.2f})')
+
+usim = data['u'][:,k,j]
+ucon = con_tc.get_time()['u_p0']
+
+measured_u_mean = np.average(ucon)
+simulated_u_mean = np.average(usim)
+print(f'measured mean u = {measured_u_mean:.3f} m/s')
+print(f'simulated mean u = {simulated_u_mean:.3f} m/s')
+print('percent error of mean u = {:.2f}%'.format(100*(simulated_u_mean - measured_u_mean) / measured_u_mean))
+print('abs error = {:.2f} m/s'.format(np.average(abs(usim - ucon))))
+
+# Step 7: save turbulence to .h5 file for input to PVade --------------------
+# ---------------------------------------------------------------------------
+
+if save_turb_files_flag:
+    h5_filename = f'pct_{sim_turb_fname[:-4]}.h5'
+
+    t_steps = con_tc.get_time().index.size
+    time = con_tc.get_time().index.values.astype(float)
+
+    with h5py.File(h5_filename, "w") as fp:
+        fp.create_dataset("time_index", shape=(t_steps,))
+        fp["time_index"][:] = time
+        
+        fp.create_dataset("y_coordinates", shape=(ny,))
+        fp["y_coordinates"][:] = y
+            
+        fp.create_dataset("z_coordinates", shape=(nz,))
+        fp["z_coordinates"][:] = z
+            
+        fp.create_dataset("u", shape=(t_steps, nz, ny))
+        fp["u"][:] = data['u'][:]
+        
+        fp.create_dataset("v", shape=(t_steps, nz, ny))
+        fp["v"][:] = data['v'][:]
+        
+        fp.create_dataset("w", shape=(t_steps, nz, ny))
+        fp["w"][:] = data['w'][:]
+    
+    print(f'saved turb to h5 file: {h5_filename}')
